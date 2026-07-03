5f9b0a442c73b9e62aab26ff0c6a36feaae5a356
Mohammed M. Salma <139702416+Mohammed-Salma@users.noreply.github.com>
2026-07-03 17:22:47 +0300

    feat: create the error message to login page view with form and validation handling

--- basic/resources/views/auth/login.blade.php
+++ basic/resources/views/auth/login.blade.php
@@ -39,15 +39,26 @@
                                 <div class="pt-0">
                                     <form method="POST" action="{{ route('login') }}" class="my-4">
                                         @csrf
+                                        @if (session('error'))
+                                            <div class="alert alert-danger">
+                                                {{ session('error') }}
+                                            </div>
+                                        @endif
                                         <div class="form-group mb-3">
                                             <label for="emailaddress" class="form-label">Email address</label>
                                             <input class="form-control" type="email" id="email" name="email"
                                                 required="" placeholder="Enter your email">
+                                            @error('email')
+                                                <span class="text-danger">{{ $message }}</span>
+                                            @enderror
                                         </div>
                                         <div class="form-group mb-3">
                                             <label for="password" class="form-label">Password</label>
                                             <input class="form-control" type="password" required="" id="password"
                                                 name="password" placeholder="Enter your password">
+                                            @error('password')
+                                                <span class="text-danger">{{ $message }}</span>
+                                            @enderror
                                         </div>
                                         <div class="form-group mb-0 row">
                                             <div class="col-12">
