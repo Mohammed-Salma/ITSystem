5ddeeed7516b43c8c53a6c79288d11720efaf7e3
Mohammed M. Salma <139702416+Mohammed-Salma@users.noreply.github.com>
2026-07-10 22:45:59 +0300

    feat: implement reviews module with database migration, model, controller, and admin sidebar, and make allReview method and route

--- basic/routes/web.php
+++ basic/routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\Backend\ReviewController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -30,4 +31,11 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [AdminController::class, 'AdminProfile'])->name('admin.profile');
     Route::post('/profile/store', [AdminController::class, 'ProfileStore'])->name('profile.store');
     Route::post('/admin/password/update', [AdminController::class, 'PasswordUpdate'])->name('admin.password.update');
+});
+
+Route::middleware('auth')->group(function () {
+
+    Route::controller(ReviewController::class)->group(function () {
+        Route::get('all/review', 'AllReview')->name('all.review');
+    });
 });
