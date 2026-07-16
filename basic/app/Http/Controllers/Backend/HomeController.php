384a7a3f7307810a1f6f4478a4239a8bc773efac
Mohammed M. Salma <139702416+Mohammed-Salma@users.noreply.github.com>
2026-07-16 14:25:27 +0300

    feat: implement CRUD operations for features and update logic for clarifi and usability sections, Home Usability Section Setup Part 2

--- basic/app/Http/Controllers/Backend/HomeController.php
+++ basic/app/Http/Controllers/Backend/HomeController.php
@@ -136,4 +136,54 @@ class HomeController extends Controller
         return view('admin.backend.usability.get_usability', compact('usability'));
     }
     //End Method
+
+
+
+    public function UpdateUsability(Request $request)
+    {
+        $usability_id = $request->id;
+        $usability = Usability::find($usability_id);
+
+        if ($request->file('image')) {
+            $image = $request->file('image');
+            $manager = new ImageManager(new Driver()); // Install intervention/image first
+
+            $name_gen = hexdec(uniqid()) . '.' . $image->getClientOriginalExtension();
+            $img = $manager->read($image);
+            $img->resize(560, 400)->save('upload/usability/' . $name_gen);
+            $save_url = 'upload/usability/' . $name_gen;
+
+            //Delete Old Image
+            if (file_exists(public_path($usability->image))) {
+                @unlink(public_path($usability->image));
+            }
+
+            Usability::find($usability_id)->update([
+                'title' => $request->title,
+                'description' => $request->description,
+                'youtube' => $request->youtube,
+                'link' => $request->link,
+                'image' => $save_url,
+            ]);
+            $notification = array(
+                'message' => 'Usability Updated With image Successfully',
+                'alert-type' => 'success'
+            );
+            return redirect()->back()->with($notification);
+        } else {
+            Usability::find($usability_id)->update([
+                'title' => $request->title,
+                'description' => $request->description,
+                'youtube' => $request->youtube,
+                'link' => $request->link,
+            ]);
+            $notification = array(
+                'message' => 'Usability Updated Without image Successfully',
+                'alert-type' => 'success'
+            );
+            return redirect()->back()->with($notification);
+        }
+    }
+    //End Method
+
 }
