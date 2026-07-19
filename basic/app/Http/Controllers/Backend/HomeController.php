0c5d91fb42ed7226f68d4dc6151d57a6e92be7ba
Mohammed M. Salma <139702416+Mohammed-Salma@users.noreply.github.com>
2026-07-19 15:48:18 +0300

    feat: implement HomeController to manage app media updates, Home Answers Section Setup Part 3

--- basic/app/Http/Controllers/Backend/HomeController.php
+++ basic/app/Http/Controllers/Backend/HomeController.php
@@ -342,5 +342,38 @@ class HomeController extends Controller
     }
     //End Method
 
+    public function UpdateAppsImage(Request $request, $id)
+    {
+        $apps = App::findOrFail($id);
+
+        if ($request->file('image')) {
+            $image = $request->file('image');
+            $manager = new ImageManager(new Driver()); // Install intervention/image first
+
+            $name_gen = hexdec(uniqid()) . '.' . $image->getClientOriginalExtension();
+            $img = $manager->read($image);
+            $img->resize(306, 481)->save(public_path('upload/apps/' . $name_gen));
+            $save_url = 'upload/apps/' . $name_gen;
+
+            if (file_exists(public_path($apps->image))) {
+                @unlink(public_path($apps->image));
+            }
+            $apps->update([
+                'image' => $save_url,
+            ]);
+            return response()->json([
+                'success' => true,
+                'image_url' => asset($save_url),
+                'message' => 'Image Updated Successfully',
+            ]);
+        }
+
+        return response()->json([
+            'success' => false,
+            'message' => 'Image Upload Failed!',
+        ], 400);
+    }
+    //End Method
+
 
 }
