54ac7adf9667729e6307d0722ef6d22afd876151
Mohammed M. Salma <139702416+Mohammed-Salma@users.noreply.github.com>
2026-07-29 13:56:44 +0300

    feat: implement team management features including listing, editing, and controller logic with image processing, Our Team Section Setup Part 3

--- basic/app/Http/Controllers/Backend/TeamController.php
+++ basic/app/Http/Controllers/Backend/TeamController.php
@@ -48,4 +48,71 @@ class TeamController extends Controller
         return redirect()->route('all.team')->with($notification);
     }
     //End Method
+
+    public function EditTeam($id)
+    {
+        $team = Team::find($id);
+        return view('admin.backend.team.edit_team', compact('team'));
+    }
+    //End Method
+
+    public function UpdateTeam(Request $request)
+    {
+        $team_id = $request->id;
+        $team = Team::find($team_id);
+
+
+        if ($request->file('image')) {
+            $image = $request->file('image');
+            $manager = new ImageManager(new Driver()); // Install intervention/image first
+
+            $name_gen = hexdec(uniqid()) . '.' . $image->getClientOriginalExtension();
+            $img = $manager->read($image);
+            $img->resize(306, 400)->save('upload/team/' . $name_gen);
+            $save_url = 'upload/team/' . $name_gen;
+
+            //to remove and change old image
+            if (file_exists(public_path($team->image))) {
+                unlink(public_path($team->image));
+            }
+
+            Team::find($team_id)->update([
+                'name' => $request->name,
+                'position' => $request->position,
+                'image' => $save_url,
+            ]);
+            $notification = array(
+                'message' => 'Team Updated With image Successfully',
+                'alert-type' => 'success'
+            );
+            return redirect()->route('all.team')->with($notification);
+        } else {
+            Team::find($team_id)->update([
+                'name' => $request->name,
+                'position' => $request->position,
+            ]);
+            $notification = array(
+                'message' => 'Team Updated Without image Successfully',
+                'alert-type' => 'success'
+            );
+            return redirect()->route('all.team')->with($notification);
+        }
+    }
+    //End Method
+
+    public function DeleteTeam($id)
+    {
+        $team = Team::find($id);
+        $img = $team->image;
+        unlink($img);
+
+        Team::find($id)->delete();
+
+        $notification = array(
+            'message' => 'Team Deleted Successfully',
+            'alert-type' => 'success'
+        );
+        return redirect()->back()->with($notification);
+    }
+    //End Method
 }
